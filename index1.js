9690fc060e198ae1e04489eeea2b3201f17e5e9b
ZaibaElmi08 <96732111+ZaibaElmi08@users.noreply.github.com>
2023-11-20 11:25:39 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,40 @@
+const WebSocket = require('ws');
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// WebSocket connection handling
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Handle messages from clients
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+
+    // Broadcast the message to all clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle connection close
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
